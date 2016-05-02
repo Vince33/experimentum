746f0a4601c5080d36789a52737ef08a57a2368b
Vince Hines <vinjh3@gmail.com>
2016-05-01 23:31:02 -0400

    adds logo letter

--- header.php
+++ header.php
@@ -23,8 +23,28 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'experimentum' ); ?></a>
+	<?php if ( get_header_image() ) { ?>
+		<header id="masthead" class="site-header" style="background-image:url(<?php header_image(); ?>)" role="banner">
+	<?php } else { ?>
+		<header id="masthead" class="site-header" role="banner">
+	<?php } ?>
+	
 
-	<header id="masthead" class="site-header" role="banner">
+		<?php // Display site icon or first letter as logo ?>	
+		<div class="site-logo">
+			<?php $site_title = get_bloginfo( 'name' ); ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<div class="screen-reader-text">
+					<?php printf( esc_html__('Go to the home page of %1$s', 'popperscores'), $site_title ); ?>	
+				</div>
+				<div class="site-firstletter" aria-hidden="true">
+					<?php echo substr($site_title, 0, 1); ?>
+				</div>
+			</a>
+		</div>
+
+
+
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -24,8 +24,8 @@ function experimentum_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'experimentum_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'width'                  => 1600,
+		'height'                 => 420,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'experimentum_header_style',
 	) ) );

cefeacd2861635ca1565fdb3fa9a92f74b4baac2
Vince Hines <vinjh3@gmail.com>
2016-05-06 08:46:59 -0400

    pulls in and adjust some scripts for the menu

--- functions.php
+++ functions.php
@@ -122,7 +122,11 @@ function experimentum_scripts() {
 
 	
 
-	wp_enqueue_script( 'experimentum-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'experimentum-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
+	wp_localize_script( 'experimentum-navigation', 'screenReaderText', array(
+		'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'experimentum' ) . '</span>',
+		'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'experimentum' ) . '</span>',
+	) );
 
 	wp_enqueue_script( 'experimentum-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
